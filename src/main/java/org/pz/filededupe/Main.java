f08912ffd0069e2da82e47503a00eae092b28502
platypusguy <platypus.software@gmail.com>
2020-08-08 06:57:15 -0700

    Cleaned up Javadoc, updated version number to 1.0, and got rid of minor warnings

--- src/main/java/org/pz/filededupe/Main.java
+++ src/main/java/org/pz/filededupe/Main.java
@@ -1,6 +1,6 @@
 /*
  * Looks for duplicate files based on CRC-32 checksumming. 
- * Project requires JDK 11 or later.
+ * Project requires JDK 8 or later.
  *
  * Copyright (c) 2017-20 by Andrew Binstock. All rights reserved.
  * Licensed under the Creative Commons Attribution, Share Alike license
@@ -11,6 +11,7 @@ package org.pz.filededupe;
 import java.util.*;
 
 /**
+ * The main line. All processing starts here.
  * @author alb (Andrew Binstock @platypusguy)
  */
 public class Main {
@@ -76,12 +77,17 @@ public class Main {
         System.out.println( "Number of duplicates found: " + dupesCount );
     }
 
-    private static void printCopyright()
-    {
+    /**
+     * Simply prints the copyright
+     */
+    private static void printCopyright() {
         System.out.println(
-            "FileDedupe v.0.3 (c) Copyright 2017-20 Andrew Binstock. All rights reserved.\n" );
+            "FileDedupe v.1.0 (c) Copyright 2017-20 Andrew Binstock. All rights reserved.\n" );
     }
 
+    /**
+     * Explains program usage
+     */
     private static void showUsage() {
 
         System.out.println(
